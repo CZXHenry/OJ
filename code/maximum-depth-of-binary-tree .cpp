40886941538feb8f18614364ebb5ab76d4c263c0
再兴 <@HENRY>
2017-07-05 15:02:43 +0800

    nowcode leetcode 148

---
+++ code/maximum-depth-of-binary-tree .cpp
@@ -0,0 +1,26 @@
+/**
+* Definition for binary tree
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	int maxDepth(TreeNode *root) {
+		if (root == null)
+		{
+			return 0;
+		}
+		else
+		{
+			int left_depth;
+			int right_depth;
+			left_depth = maxDepth(root->left);
+			right_depth = maxDepth(root->right);
+			return 1 + (left_depth > right_depth ? left_depth : right_depth);
+		}
+	}
+};
